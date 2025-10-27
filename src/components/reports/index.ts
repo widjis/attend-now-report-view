2a64ee24119d55e1a077a35c7fca558eeb50d512
widjis <widji.santoso@gmail.com>
2025-10-27 15:26:05 +0800

    style(reports): fix missing newline at end of file

--- src/components/reports/index.ts
+++ src/components/reports/index.ts
@@ -2,3 +2,4 @@ export { default as ReportGeneration } from './reportgeneration';
 export { default as ReportHistory } from './ReportHistory';
 export { default as ReportStatistics } from './ReportStatistics';
 export { default as WhatsAppSettings } from './WhatsAppSettings';
+ 
