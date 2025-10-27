0b73e0fabe1d3ec6a922d57642a43d397004a11a
widjis <widji.santoso@gmail.com>
2025-10-27 15:24:57 +0800

    Fix ReportGeneration import path in index.ts to match Docker server filename

--- src/components/reports/index.ts
+++ src/components/reports/index.ts
@@ -1,4 +1,4 @@
-export { default as ReportGeneration } from './ReportGeneration';
+export { default as ReportGeneration } from './reportgeneration';
 export { default as ReportHistory } from './ReportHistory';
 export { default as ReportStatistics } from './ReportStatistics';
 export { default as WhatsAppSettings } from './WhatsAppSettings';
