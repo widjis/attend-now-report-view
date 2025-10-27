aa84894596bb8bb369195774b8f87f8f02f6eecb
widjis <widji.santoso@gmail.com>
2025-10-27 11:53:34 +0800

    Fix module imports in reports index.ts - remove file extensions for Docker build compatibility

--- src/components/reports/index.ts
+++ src/components/reports/index.ts
@@ -1,4 +1,4 @@
-export { default as ReportGeneration } from './ReportGeneration.tsx';
-export { default as ReportHistory } from './ReportHistory.tsx';
-export { default as ReportStatistics } from './ReportStatistics.tsx';
-export { default as WhatsAppSettings } from './WhatsAppSettings.tsx';
+export { default as ReportGeneration } from './ReportGeneration';
+export { default as ReportHistory } from './ReportHistory';
+export { default as ReportStatistics } from './ReportStatistics';
+export { default as WhatsAppSettings } from './WhatsAppSettings';
